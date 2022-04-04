5b8098c169bebcdd21a1619257842013507e2f21
Chistaykov Aleksey <chskela@gmail.com>
2022-04-04 21:16:35 +0300

    Added Color.kt

---
+++ src/main/kotlin/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF519BD0)
+val Secondary = Color(0xFF858585)
+val Background = Color(0xFFF5F5F5)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
